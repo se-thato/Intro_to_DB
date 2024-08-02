81f7685b91d8e94f3bb75e18c91f9c2d17b35317
se-thato <thatoselepe80@gmail.com>
2024-08-02 17:35:22 +0200

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -33,7 +33,6 @@ CRFEATE TABLE Order_Details (
     orderdetail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    
     FOREIGN KEY(order_id) REFERENCES Orders(order_id),
     FOREIGN KEY(book_id) REFERENCES Books(book_id)
 );

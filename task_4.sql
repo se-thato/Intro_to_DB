a6edfc41f3887dff9e80410d8a346088bc7540f0
se-thato <thatoselepe80@gmail.com>
2024-08-02 19:08:41 +0200

    Update task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,12 @@
+SELECT
+    book_id,
+    INT,
+    ,NOT NULL
+    PRIMARY_KEY,
+    utf8,
+    EXTRA
+FROM
+    alx_book_store.book_id, title, author_id, price, publication_date
+WHERE
+    Books= DATABASE()
+    AND Books = "Books";

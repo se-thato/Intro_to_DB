08b22c729ed48ae15c9669f00b9fd5abdb92b893
se-thato <thatoselepe80@gmail.com>
2024-08-02 15:44:36 +0200

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -27,7 +27,7 @@ CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CRFEATE TABLE Order_Details (

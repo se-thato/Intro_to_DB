372f34adb19bf35b29efcb4e399fd63781b9836c
se-thato <thatoselepe80@gmail.com>
2024-08-02 20:46:38 +0200

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,2 @@
-INSERT INTO Customer
-VALUES(1, "Cole Baidoo", "cbaidoo@sandtech.com", 123 Happiness Ave.)
-
-SELECT * FROM Customer;
+INSERT INTO Customer (customer_id, customer_name, email,address)
+VALUES(1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.")

81266727a36be76c195a669740f7f3063f30318c
se-thato <thatoselepe80@gmail.com>
2024-08-02 19:15:24 +0200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,12 +1,12 @@
 SELECT
     book_id,
     INT,
-    ,NOT NULL
+    NOT NULL
     PRIMARY_KEY,
     utf8,
     EXTRA
 FROM
-    alx_book_store.book_id, title, author_id, price, publication_date
+    INFORMATION_SCHEMA.book_id, title, author_id, price, publication_date
 WHERE
-    Books= DATABASE()
-    AND Books = "Books";
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books';
